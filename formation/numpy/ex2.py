1987cb06a4044ef6976e7f2a7a28dcbf81e83b50
antoine munoz <antoine.munoz@epitech.eu>
2024-02-13 16:26:33 +0100

    numpy ex 2

---
+++ formation/numpy/ex2.py
@@ -0,0 +1,20 @@
+import numpy as np
+from scipy import misc
+import matplotlib.pyplot as plt
+
+def zoom(image, factor):
+    x, y = image.shape
+    newX = int(x * factor)
+    newY = int(y * factor)
+
+    image = image[newX:x - newX, newY:y - newY]
+    plt.imshow(image, cmap=plt.cm.gray)
+    plt.show()
+
+def main():
+    face = misc.face(gray=True)
+    plt.imshow(face, cmap=plt.cm.gray)
+    zoom(face, 0.25)
+
+if __name__ == "__main__":
+    main()
